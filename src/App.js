a8ef600fd44edf6ec0fb614df03cbf98c7412436
Adriano Misina <adriano.misina@gmail.com>
2021-09-10 10:02:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// usando classe
+// class App extends React.Component {
+//   render() {
+//     return <h1>Testando 1,2,3....</h1>;
+//   }
+// }
+
+// usando função
+// function App() {
+//   return <h1>Testando 3, 4, 5</h1>
+// }
+
+// criando com função arrow
+// let App = () => {
+//   return <h1>Testando 7, 8, 9 ...</h1>
+// }
+
+// criando com arrow function e funçao anonima
+let App = () => <h1>Testando 10, 11, 12....</h1>
 
 export default App;
