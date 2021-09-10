e489b605e4a8928de30d42bf165cd96e6c576286
Adriano Misina <adriano.misina@gmail.com>
2021-09-10 15:10:02 -0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 
-// usando classe
-// class App extends React.Component {
-//   render() {
-//     return <h1>Testando 1,2,3....</h1>;
-//   }
-// }
+function formatarNome(usuario) {
+    return usuario.nome + ' ' + usuario.sobrenome;
+}
 
-// usando função
-// function App() {
-//   return <h1>Testando 3, 4, 5</h1>
-// }
+function App() {
+    // let usuario = {
+    //     nome: 'Adriano',
+    //     sobrenome: 'Misina'
+    // }
 
-// criando com função arrow
-// let App = () => {
-//   return <h1>Testando 7, 8, 9 ...</h1>
-// }
-
-// criando com arrow function e funçao anonima
-let App = () => <h1>Testando 10, 11, 12....</h1>
+    let imagem = ' http://www.google.com.br/google.jpg';
+ 
+    return <>
+        {/* <div>Meu nome é {formatarNome(usuario)}</div> */}
+        <img src={imagem} />
+    </>;
+}
 
 export default App;
+
